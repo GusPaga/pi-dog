0eeaecab4323d19660d11c5253db979c4165b417
Gustavo Pagano <gustavopagano03@gmail.com>
2023-11-21 19:55:23 +0100

    modifications on get method

--- api/src/controllers/getDog.js
+++ api/src/controllers/getDog.js
@@ -1,5 +1,5 @@
-const { Temperament, Dog } = require('../db');
-const axios = require('axios');
+const { Temperament, Dog } = require("../db");
+const axios = require("axios");
 const { apiKey } = process.env;
 
 // [ ] GET /dogs:
@@ -7,60 +7,72 @@ const { apiKey } = process.env;
 // Debe devolver solo los datos necesarios para la ruta principal
 
 const getDog = async (req, res) => {
-    try {
-        let findDbDog = await Dog.findAll({
-            include: {
-                model: Temperament
-            }
-        });
-        //console.log('findDbDog', findDbDog);
+  try {
+    let findDbDog = await Dog.findAll({
+      include: {
+        model: Temperament,
+      },
+    });
+    //console.log('findDbDog', findDbDog);
 
-        let dbDog = findDbDog.map((e) => {
-            return {
-                id: e.id,
-                name: e.name,
-                min_weight: e.min_weight,
-                max_weight: e.max_weight,
-                min_height: e.min_height,
-                max_height: e.max_height,
-                life: e.life,
-                image: e.image,
-                createdOnDb: e.createdOnDb,
-                temperament: (e.temperaments?.map(e => e.name)).toString().replace(/,/g,", ")
-            }
-        })
+    let dbDog = findDbDog.map((e) => {
+      return {
+        id: e.id,
+        name: e.name,
+        min_weight: e.min_weight,
+        max_weight: e.max_weight,
+        min_height: e.min_height,
+        max_height: e.max_height,
+        life: e.life,
+        image: e.image,
+        createdOnDb: e.createdOnDb,
+        temperament: e.temperaments
+          ?.map((e) => e.name)
+          .toString()
+          .replace(/,/g, ", "),
+      };
+    });
 
-        const api = await axios.get(`https://api.thedogapi.com/v1/breeds?api_key=${apiKey}`)
-        const data = api.data;
+    const api = await axios.get(
+      `https://api.thedogapi.com/v1/breeds?api_key=${apiKey}`
+    );
+    const data = api.data;
 
-        let filtrated = data.map((e) => {
-            return {
-                id: e.id ? e.id : 'not available',
-                name: e.name ? e.name : 'not available',
-                min_weight: e.weight.metric.split(' -')[0] ? Number(e.weight.metric.split(' -')[0]) : 0,
-                max_weight: e.weight.metric.split(' -')[1] ? Number(e.weight.metric.split(' -')[1]) : 0,
-                min_height: e.height.metric.split(' -')[0] ? Number(e.height.metric.split(' -')[0]) : 0,
-                max_height: e.height.metric.split(' -')[1] ? Number(e.height.metric.split(' -')[1]) : 0,
-                life: e.life_span ? e.life_span : 'not available',
-                image: e.image.url ? e.image.url : 'not available',
-                createdOnDb: e.createdOnDb? createdOnDb: false,
-                temperament: e.temperament ? e.temperament : 'not available',
-                origin: e.origin? e.origin: 'not available'
-            }
-        })
+    let filtrated = data.map((e) => {
+      return {
+        id: e.id ? e.id : "not available",
+        name: e.name ? e.name : "not available",
+        min_weight: e.weight.metric.split(" -")[0]
+          ? Number(e.weight.metric.split(" -")[0])
+          : 0,
+        max_weight: e.weight.metric.split(" -")[1]
+          ? Number(e.weight.metric.split(" -")[1])
+          : 0,
+        min_height: e.height.metric.split(" -")[0]
+          ? Number(e.height.metric.split(" -")[0])
+          : 0,
+        max_height: e.height.metric.split(" -")[1]
+          ? Number(e.height.metric.split(" -")[1])
+          : 0,
+        life: e.life_span ? e.life_span : "not available",
+        //image: e.image.url ? e.image.url : "not available",
+        createdOnDb: e.createdOnDb ? createdOnDb : false,
+        temperament: e.temperament ? e.temperament : "not available",
+        origin: e.origin ? e.origin : "not available",
+      };
+    });
 
-        //console.log(filtrated)
-        let allDogs = filtrated.concat(dbDog);
-        res.send(allDogs);
+    //console.log("filtrated", filtrated);
+    let allDogs = filtrated.concat(dbDog);
+    res.send(allDogs);
 
-        console.log(`total Api ${data.length}`);
-        console.log(`total DB ${dbDog.length}`);
-        console.log(`total Dogs ${allDogs.length}`);
-
-    } catch (error) {
-        res.status(400).send({ "Fail getDog": error })
-    }
-
-}
+    console.log(`total Api ${data.length}`);
+    console.log(`total DB ${dbDog.length}`);
+    console.log(`total Dogs ${allDogs.length}`);
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({ "Fail getDog": error });
+  }
+};
 
 module.exports = { getDog };
--- api/src/controllers/nodify.js
+++ api/src/controllers/nodify.js
@@ -1,26 +1,37 @@
-const { Temperament, Dog } = require('../db');
+const { Temperament, Dog } = require("../db");
 
 const modify = async (req, res) => {
-    try {
-        const { name, min_weight, max_weight, min_height, max_height, life, image, createdOnDb, temperament } = req.body;
+  try {
+    const {
+      name,
+      min_weight,
+      max_weight,
+      min_height,
+      max_height,
+      life,
+      image,
+      createdOnDb,
+      temperament,
+    } = req.body;
 
-        await Dog.update({
-            name,
-            min_weight,
-            max_weight,
-            min_height,
-            max_height,
-            life,
-            image,
-            temperament,
-        }, { where: { id } });
+    await Dog.update(
+      {
+        name,
+        min_weight,
+        max_weight,
+        min_height,
+        max_height,
+        life,
+        image,
+        temperament,
+      },
+      { where: { id } }
+    );
 
-        res.send('Dog modificated');
-
-    } catch (error) {
-        res.status(400).send({ "Fail modify": error })
-    }
-
+    res.send("Dog modificated");
+  } catch (error) {
+    res.status(400).send({ "Fail modify": error });
+  }
 };
 
 module.exports = { modify };
